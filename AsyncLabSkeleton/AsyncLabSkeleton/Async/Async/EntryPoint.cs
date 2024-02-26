5acc8ce849053575dba6acf6699b036f7e14eac0
Joshua Salijeni <salijenijoshuq@gmail.com>
2024-02-26 18:08:25 +0000

    Async and Task
    Moving on from threading

---
+++ AsyncLabSkeleton/AsyncLabSkeleton/Async/Async/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using Client;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+// Add code here to send requests
+
+//ClientBehaviours.SendRequest("Hello World","TaskOne");
+//ClientBehaviours.SendRequest("Hello World", "TaskTwo");
+//ClientBehaviours.SendRequest("Hello World", "TaskThree");
+
+Task one = ClientBehaviours.SendRequest("Hello World", "TaskOne");
+Task two = ClientBehaviours.SendRequest("Hello World", "TaskTwo");
+Task three = ClientBehaviours.SendRequest("Hello World", "TaskThree");
+
+Stopwatch sw = new Stopwatch();
+one.Wait();
+
+Console.WriteLine("Execution finished");
+Console.ReadLine();
---
+++ AsyncLabSkeleton/AsyncLabSkeleton/Async/Server/EntryPoint.cs
@@ -0,0 +1,2 @@
+﻿using Server;
+Server.ServerBehaviours.StartServer();
